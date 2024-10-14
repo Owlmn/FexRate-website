edcc905142876e0d22a24d770ecdbcd7fe394a0d
Owlmn <tregchannel@yandex.ru>
2024-10-15 01:39:50 +1000

    Восстановление файлов

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <nav>
+          <Link href="/"> Конвертация </Link>
+          <Link href="/graphics"> Графики </Link>
+      </nav>
         {children}
       </body>
     </html>
