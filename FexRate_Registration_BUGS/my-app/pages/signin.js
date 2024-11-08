64c2a613a6d16b13745f9c13eba0211a60846951
MiroshZ <68206729+MiroshZ@users.noreply.github.com>
2024-11-08 12:28:17 +1000

    Add files via upload
    Белая полоса вокруг контейнера 
    Разные размеры строки ввода почты и пароля 
    Не кликабельная ссылка на регистрацию
    (Пункт "назад" съехал)
    
    Signed-off-by: MiroshZ <68206729+MiroshZ@users.noreply.github.com>

---
+++ FexRate_Registration_BUGS/my-app/pages/signin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthorizationSignIn from '../app/components/AuthorizationSignIn';
+
+const SignInPage = () => {
+  return <AuthorizationSignIn />;
+};
+
+export default SignInPage;
