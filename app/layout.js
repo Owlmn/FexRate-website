1ae0826e1c63f42be9111fb1f4adab1cad5e63d8
Elimir Tilindis <elindisof@gmail.com>
2024-12-10 12:11:37 +1000

    убрал футер, фикс отступов

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,22 @@
 "use client";
 
 import "./styles/globals.css";
-import { usePathname } from 'next/navigation';
-import {Header} from "@/app/components/Common/Header/Header";
-import {Footer} from "@/app/components/Common/Footer/Footer";
+import { usePathname } from "next/navigation";
+import { Header } from "@/app/components/Common/Header/Header";
+import { Footer } from "@/app/components/Common/Footer/Footer";
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname();
-    return (
-        <html lang="en">
-        <body>
-          {pathname !== '/login_page' && <Header />}
-          <main>{children}</main>
-          {pathname !== '/login_page' && <Footer />}
-        </body>
-        </html>
-    );
+  const pathname = usePathname();
+  const shouldShowFooter =
+    pathname !== "/login_page" && pathname !== "/personal_account";
+
+  return (
+    <html lang="en">
+      <body>
+        {pathname !== "/login_page" && <Header />}
+        <main>{children}</main>
+        {shouldShowFooter && <Footer />}
+      </body>
+    </html>
+  );
 }
