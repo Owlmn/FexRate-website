9915b1d3a701cd0188672294c771a40d1015c03c
Elimir Tilindis <elindisof@gmail.com>
2024-12-10 16:36:15 +1000

    буб

---
+++ app/forget_password2/page.js
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react';
+import AuthorizationSignIn from './components/AuthorizationSignIn';
+
+export default function SignInPage() {
+    return (
+        <main>
+            <AuthorizationSignIn />
+        </main>
+    );
+}
