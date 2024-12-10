e8340e64594748f63f1d200fb30c8a0e6dd5a788
Elimir Tilindis <elindisof@gmail.com>
2024-12-10 16:52:13 +1000

    буб3

--- app/layout.js
+++ app/layout.js
@@ -7,12 +7,39 @@ import { Footer } from "@/app/components/Footer/Footer";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
+
+  // Array of paths where header should be disabled
+  const noHeaderPaths = [
+    // "/login_page",
+    "/sign_in",
+    "/sign_up",
+    "/sign_up_apr",
+    "/forget_password1",
+    "/forget_password2",
+    "/forget_password3",
+  ];
+
+  // Array of paths where footer should be disabled
+  const noFooterPaths = [
+    // "/login_page",
+    "/sign_in",
+    "/sign_up",
+    "/sign_up_apr",
+    "/forget_password1",
+    "/forget_password2",
+    "/forget_password3",
+    "/personal_account",
+  ];
+
+  const shouldShowHeader = !noHeaderPaths.includes(pathname);
+  const shouldShowFooter = !noFooterPaths.includes(pathname);
+
   return (
     <html lang="en">
       <body>
-        {pathname !== "/login-page" && <Header />}
+        {shouldShowHeader && <Header />}
         <main>{children}</main>
-        <Footer />
+        {shouldShowFooter && <Footer />}
       </body>
     </html>
   );
