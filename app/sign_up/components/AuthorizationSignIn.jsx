defe731846b48ac5f740ea8b82c5d68086a0a493
Elimir Tilindis <elindisof@gmail.com>
2024-12-10 17:29:05 +1000

    сделаны страницы войти и регистрация

--- app/sign_up/components/AuthorizationSignIn.jsx
+++ app/sign_up/components/AuthorizationSignIn.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
-import styles from './AuthorizationSignIn.module.css';
+"use client";
+
+import React, { useState } from "react";
+import styles from "./AuthorizationSignIn.module.css";
 import Link from "next/link";
-import GlobalStyles from './globals.css';
+import { useRouter } from "next/navigation";
 
 const AuthorizationSignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isChecked, setIsChecked] = useState(false); // Новое состояние для галочки
+  const router = useRouter();
 
   const handleSubmit = (e) => {
     e.preventDefault();
- 
+    // Перенаправление на страницу /personal_account при успешной отправке формы
+    router.push("/personal_account");
+  };
+
+  const handleRegisterClick = (e) => {
+    e.preventDefault();
+    // Перенаправление на страницу /sign_up при клике на "Зарегистрироваться"
+    router.push("/sign_in");
+  };
+
+  const handleForgotPasswordClick = (e) => {
+    e.preventDefault();
+    // Перенаправление на страницу /forget_password1 при клике на "Забыли пароль?"
+    router.push("/forget_password1");
   };
 
   const togglePasswordVisibility = () => {
@@ -21,19 +38,21 @@ const AuthorizationSignIn = () => {
     <main className={styles.container}>
       <div className={styles.content}>
         <header className={styles.header}>
-          <Link href="/"> <h1 className={styles.logo}>FexRate</h1></Link>
-          {/* <nav className={styles.navItem}>
-            <img src="/images/home-icon.svg" alt="" className={styles.navIcon} />
-            <span>Главная</span>
-          </nav> */}
+          <Link href="/">
+            <h1 className={styles.logo}>FexRate</h1>
+          </Link>
         </header>
-        <h2 className={styles.title}>Войти</h2>
+        <h2 className={styles.title}>Зарегистрироваться</h2>
         <p className={styles.registerPrompt}>
-          До сих пор нет аккаунта?{" "}
-          <span className={styles.registerLink}>Зарегистрироваться</span>
+          Уже есть аккаунт?{" "}
+          <Link href="/sign_up" onClick={handleRegisterClick}>
+            <span className={styles.registerLink}>Войти</span>
+          </Link>
         </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email" className={styles.visuallyHidden}>Почта</label>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label htmlFor="email" className={styles.visuallyHidden}>
+            Почта
+          </label>
           <input
             type="email"
             id="email"
@@ -44,7 +63,9 @@ const AuthorizationSignIn = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
           <div className={styles.passwordContainer}>
-            <label htmlFor="password" className={styles.visuallyHidden}>Пароль</label>
+            <label htmlFor="password" className={styles.visuallyHidden}>
+              Пароль
+            </label>
             <input
               type={showPassword ? "text" : "password"}
               id="password"
@@ -61,17 +82,39 @@ const AuthorizationSignIn = () => {
               aria-label={showPassword ? "Скрыть пароль" : "Показать пароль"}
             >
               <img
-                src={showPassword ? "/images/hide-password.svg" : "/images/show-password.svg"}
+                src={
+                  showPassword
+                    ? "https://cdn.builder.io/api/v1/image/assets/449dd0928ae94f98b3877c3e4f2d9f26/70d9f2526071f2b4889833ea5bffaecb3aba9325d5d0f516da04abb3a1f4e426?apiKey=449dd0928ae94f98b3877c3e4f2d9f26&"
+                    : "https://cdn.builder.io/api/v1/image/assets/449dd0928ae94f98b3877c3e4f2d9f26/70d9f2526071f2b4889833ea5bffaecb3aba9325d5d0f516da04abb3a1f4e426?apiKey=449dd0928ae94f98b3877c3e4f2d9f26&"
+                }
                 alt=""
                 className={styles.passwordIcon}
               />
             </button>
           </div>
+          <div className={styles.checkboxContainer}>
+            <input
+              type="checkbox"
+              id="agreement"
+              checked={isChecked}
+              onChange={() => setIsChecked(!isChecked)}
+              className={styles.checkboxInput}
+            />
+            <label htmlFor="agreement" className={styles.checkboxLabel}>
+              Нажимая, я соглашаюсь с политикой и правилами компании
+            </label>
+          </div>
           <button type="submit" className={styles.submitButton}>
-            Войти
+            Зарегистрироваться
           </button>
         </form>
-        <a href="#" className={styles.forgotPassword}>Забыли пароль?</a>
+        <a
+          href="#"
+          className={styles.forgotPassword}
+          onClick={handleForgotPasswordClick}
+        >
+          Забыли пароль?
+        </a>
       </div>
     </main>
   );
