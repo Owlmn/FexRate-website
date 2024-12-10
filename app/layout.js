70af0fb994f58e1893ffa5db61e0f1a8bb113774
Elimir Tilindis <elindisof@gmail.com>
2024-12-10 16:42:05 +1000

    буб2

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,19 @@
 "use client";
 
 import "./globals.css";
-import { usePathname } from 'next/navigation';
-import {Header} from "@/app/components/Header/Header";
-import {Footer} from "@/app/components/Footer/Footer";
+import { usePathname } from "next/navigation";
+import { Header } from "@/app/components/Header/Header";
+import { Footer } from "@/app/components/Footer/Footer";
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname();
-    return (
-        <html lang="en">
-        <body>
-          {pathname !== '/login-page' && <Header />}
-          <main>{children}</main>
-          <Footer />
-
-        </body>
-        </html>
-    );
+  const pathname = usePathname();
+  return (
+    <html lang="en">
+      <body>
+        {pathname !== "/login-page" && <Header />}
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
