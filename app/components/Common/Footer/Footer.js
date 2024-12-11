39961662a408a7b8fa69163c09f347aa76a5c728
Elimir Tilindis <elindisof@gmail.com>
2024-12-11 12:15:42 +1000

    окнчательный фикс футера + адаптив для мобилки

--- app/components/Common/Footer/Footer.js
+++ app/components/Common/Footer/Footer.js
@@ -9,56 +9,56 @@ export function Footer({ children }) {
     <div>
       <div className="footer-box">
         <div className="footer-container">
-        <div className="block_1">
-          <div className="list">
-            <Link href="/" className="f-title">
-              FexRate
-            </Link>
-            <div className="text1">Создано FexRate Team</div>
-            <div className="logo">
-              <a href="https://t.me/DerekHax">
-                <Image src={Image1} alt="" />
-              </a>
-              <a href="https://github.com/Owlmn/FexRate-website">
-                <Image src={Image2} alt="" />
-              </a>
+          <div className="block_1">
+            <div className="list">
+              <Link href="/" className="f-title">
+                FexRate
+              </Link>
+              <div className="text1">Создано FexRate Team</div>
+              <div className="logo">
+                <a href="https://t.me/DerekHax">
+                  <Image src={Image1} alt="" />
+                </a>
+                <a href="https://github.com/Owlmn/FexRate-website">
+                  <Image src={Image2} alt="" />
+                </a>
+              </div>
+              <div className="text2c">
+                Copyright © 2024 FexRate Team. All Rights Reserved
+              </div>
             </div>
-            <div className="text2">
-              Copyright © 2024 FexRate Team. All Rights Reserved
+          </div>
+          <div className="block_2">
+            <div className="list2">
+              <Link href="/" className="f2-title">
+                FexRate
+              </Link>
+              <ul className="text2">
+                <Link href="/">
+                  <li>Конвертация</li>
+                </Link>
+                <Link href="/charts">
+                  <li>Графики</li>
+                </Link>
+                <Link href="/about">
+                  <li>О нас</li>
+                </Link>
+                <Link href="/partnership">
+                  <li>Сотрудничество</li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+          <div className="block_3">
+            <div className="list2">
+              <div className="f2-title">Информация</div>
+              <ul className="text2">
+                <li>Поддержка</li>
+                <li>Правила</li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="block_2">
-          <div className="list2">
-            <Link href="/" className="f2-title">
-              FexRate
-            </Link>
-            <ul className="text2">
-              <Link href="/">
-                <li>Конвертация</li>
-              </Link>
-              <Link href="/charts">
-                <li>Графики</li>
-              </Link>
-              <Link href="/about">
-                <li>О нас</li>
-              </Link>
-              <Link href="/partnership">
-                <li>Сотрудничество</li>
-              </Link>
-            </ul>
-          </div>
-        </div>
-        <div className="block_3">
-          <div className="list2">
-            <div className="f2-title">Информация</div>
-            <ul className="text2">
-              <li>Поддержка</li>
-              <li>Правила</li>
-            </ul>
-          </div>
-        </div>
-      </div>
       </div>
       {children}
     </div>
